416b0950254a207f5134e217d89b7543c62c8538
filip <filip1folga@gmail.com>
2025-09-30 10:41:33 +0200

    create models, urls

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+def movie_list(request):
+    pass
 
-# Create your views here.
+def movie_detail(request, pk):
+    pass
+
+def movie_create(request):
+    pass
+
+def movie_update(request, pk):
+    pass
+
+def movie_delete(request, pk):
+    pass
