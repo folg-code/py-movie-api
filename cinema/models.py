a3819bf5426da0325a97ab646e4fd117634271b9
filip <filip1folga@gmail.com>
2025-09-30 11:29:56 +0200

    finish the task

--- cinema/models.py
+++ cinema/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.CharField(max_length=1000)
     duration = models.IntegerField(help_text="Duration in minutes")
 
     def __str__(self):
--- cinema/views.py
+++ cinema/views.py
@@ -1,7 +1,6 @@
-import json
 
-from django.http import JsonResponse
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
@@ -9,31 +8,30 @@ from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
+
+@api_view(['GET', 'POST'])
 def movie_list(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
-        movie_data = request.body
-        movie_json = json.loads(movie_data)
-        serializer = MovieSerializer(data=movie_json)
+        serializer = MovieSerializer(data=request.data)
 
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return None
 
-
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
     if request.method == 'GET':
         movie = get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(movie)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
         movie = get_object_or_404(Movie, pk=pk)
-        update_data = json.loads(request.body)
-        serializer = MovieSerializer(movie, data=update_data, partial=True)
+        serializer = MovieSerializer(movie, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
